247c3b1215a8e304372d337691b349e0cdaf049f
DHodaniewicz <d.hodaniewicz@gmail.com>
2022-02-19 12:59:07 +0100

    Working methods getAllBooks, GeatBookById and addBook

--- src/main/java/pl/coderslab/controllers/BookController.java
+++ src/main/java/pl/coderslab/controllers/BookController.java
@@ -1,17 +1,45 @@
 package pl.coderslab.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import pl.coderslab.beans.Book;
+import pl.coderslab.services.MockBookService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+    private MockBookService mockBookService;
+
+    @Autowired
+    public BookController(MockBookService mockBookService) {
+        this.mockBookService = mockBookService;
+    }
 
     @RequestMapping("/helloBook")
     public Book helloBook() {
         return new Book(1L, "9788324631766", "Thinking in Java",
                 "Bruce Eckel", "Helion", "programming");
+    }
+
+    @RequestMapping("/all")
+    public List<Book> allBooks() {
+        return mockBookService.getAllBooks();
+    }
+
+    @RequestMapping("/{bookId}")
+    public Book getBookById(@PathVariable int bookId) {
+        return mockBookService.getBookById(bookId);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public void  addBook(@RequestParam String isbn, @RequestParam  String title, @RequestParam String publisher,
+                         @RequestParam String type, @RequestParam String author) {
+        mockBookService.addBook(isbn, title, publisher, type, author);
     }
 }
--- src/main/java/pl/coderslab/services/MockBookService.java
+++ src/main/java/pl/coderslab/services/MockBookService.java
@@ -1,4 +1,56 @@
 package pl.coderslab.services;
 
+import org.springframework.stereotype.Component;
+import pl.coderslab.beans.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class MockBookService {
+    private List<Book> list;
+    private static Long nextId = 5L;
+
+
+    public MockBookService() {
+        list = new ArrayList<>();
+        list.add(new Book(1L, "9788324631766", "Thiniking in Java", "Bruce Eckel", "Helion", "programming"));
+        list.add(new Book(2L, "9788324627738", "Rusz glowa Java.", "Sierra Kathy, Bates Bert", "Helion",
+                "programming"));
+        list.add(new Book(3L, "9780130819338", "Java 2. Podstawy", "Cay	Horstmann, Gary Cornell", "Helion",
+                "programming"));
+        list.add(new Book(4L, "9788349531742", "Droga Szamana. Etap 7: Na tropie stwórcy", "Wasilij Machanienko",
+                "Insignis", "fantasy"));
+
+    }
+
+
+
+    public List<Book> getAllBooks() {
+        return this.list;
+    }
+
+    public Book getBookById(long id) {
+        for (Book book : list) {
+            if (book.getId().equals(id)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public void addBook(String isbn, String title, String publisher, String type, String author) {
+            Book book = new Book(nextId, isbn, title, author, publisher, type);
+            list.add(book);
+            nextId++;
+    }
+
+    public void editBook(int id) {
+
+    }
+
+    public void deleteBook(int id) {
+
+    }
 }
